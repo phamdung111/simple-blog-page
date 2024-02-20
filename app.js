00a60108c8bbe7666711e005734f3b3dfe36def1
dungpham111 <dungvanx8@gmail.com>
2024-02-20 16:41:07 +0700

    Reponsive tablet screen

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const menuMobile = document.querySelector('.menu-mobile');
 const navMobile = document.querySelector('.nav-mobile');
-console.log(navMobile);
 var toggleMenuMobile = false;
 menuMobile.addEventListener('click', function() {
     toggleMenuMobile = !toggleMenuMobile;
-    console.log(toggleMenuMobile);
     if(toggleMenuMobile){
         navMobile.style.display = 'block';
     }else{
